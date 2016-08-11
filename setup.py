123e18b40ce347c6fa025cdce2c467570560bc4a
Louis DeLosSantos <louis@Louis-JWP.local>
2016-08-11 12:15:44 -0400

    update

--- setup.py
+++ setup.py
@@ -8,11 +8,6 @@ setup(
     name                = 'bg_deploy',
     version             = VERSION,
     description         = 'Blue Green Deployment Script',
-    url                 = 'https://github.com/jwplayer/operations/bg_deploy',
-    author              = 'JWPlayer DevOps Team',
-    author_email        = 'team@jwplayer.com',
-    maintainer          = 'Louis DeLosSantos',
-    maintainer_email    = 'louis@jwplayer.com',
     scripts             = [
         'scripts/bg_deploy'
     ],
